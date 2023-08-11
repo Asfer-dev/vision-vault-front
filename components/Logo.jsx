25a4e6309aa065e9ade9b875b104b52c2152e08b
Asfer Ali <asferali004@gmail.com>
2023-08-11 20:33:03 +0500

    completed footer

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -4,7 +4,10 @@ import imgLogo from "../public/assets/logo.png";
 
 export default function Logo() {
   return (
-    <Link href={"/"} className="flex gap-4 items-center z-20">
+    <Link
+      href={"/"}
+      className="flex gap-4 items-center z-20 hover:opacity-90 transition"
+    >
       <Image className="" src={imgLogo} height={40} width={40} />
       <h1 className="font-logo uppercase text-lg md:text-2xl">Vision Vault</h1>
     </Link>
